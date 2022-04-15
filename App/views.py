b477d2465505e2951bfe71d7e9c64cbac55cd872
unknown <c.hardik125@gmail.com>
2022-04-16 03:11:48 +0530

    status request

--- App/views.py
+++ App/views.py
@@ -12,27 +12,9 @@ def customer_info(request,pk):
     try:
         customer = Customer.objects.get(id=pk)
     except Customer.DoesNotExist:
-        customer = None
+        return Response(status=status.HTTP_204_NO_CONTENT)
     serializers = customerSerializer(customer)
-    return Response(serializers.data)
-
-# Add customer information
-@api_view(['POST'])
-def addcustomer(request):
-    serializers = customerSerializer(data=request.data)
-    if serializers.is_valid():
-        serializers.save()
-        return Response(serializers.data, status=status.HTTP_201_CREATED)
-    else:
-        return Response(status=status.HTTP_400_BAD_REQUEST)
-
-# Get All customers 
-@api_view(['GET'])
-def customers(request):
-    if request.method == 'GET':
-        customers = Customer.objects.all()
-        serializers = customersSerializer(customers, many=True)
-    return Response(serializers.data)
+    return Response(serializers.data,status=status.HTTP_200_OK)
 
 # Get Invoice information
 @api_view(['GET'])
@@ -40,28 +22,37 @@ def invoice_info(request):
     if request.method == 'GET':
         invoice = Invoice.objects.all().order_by('id')
         serializers = invoiceSerializer(invoice, many=True,context={'request': request})
-    return Response(serializers.data)
+    return Response(serializers.data,status=status.HTTP_200_OK)
+
+# Get All customers 
+@api_view(['GET'])
+def customers(request):
+    if request.method == 'GET':
+        customers = Customer.objects.all()
+        serializers = customersSerializer(customers, many=True)
+    return Response(serializers.data, status=status.HTTP_200_OK)
+
+
+# Add customer information
+@api_view(['POST'])
+def addcustomer(request):
+    serializers = customerSerializer(data=request.data)
+    if serializers.is_valid():
+        serializers.save()
+        return Response(serializers.data, status=status.HTTP_202_ACCEPTED)
+    else:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
 
 # Add invoice information
 @api_view(['POST'])
 def add_invoice(request):
-    serializers = addinvoiceSerializer(data=request.data)
-    
+    serializers = addinvoiceSerializer(data=request.data) 
     if serializers.is_valid():
         serializers.save()
-        return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.data, status=status.HTTP_202_ACCEPTED)
     else:
         return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
-#function for invoice autoincrement
-def increment_invoice_number():
-    last_invoice = Invoice.objects.all().order_by('id').last()
-    if not last_invoice:
-         return '01-22/23'
-    invoice_no = last_invoice.invoice_no
-    invoice_int = int(invoice_no.split('01-22/23')[-1])
-    new_invoice_int = invoice_int + 1
-    new_invoice_no = '01-22/23' + str(new_invoice_int)
-    return new_invoice_no
+
    
